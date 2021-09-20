6f6f5b838e84206736a5e496fb46d77ec29b6edb
Konrad Misterski <kmisterski@o2.pl>
2021-09-20 18:37:00 +0200

    Add RookFigure class

---
+++ inc/mygame/chess/figure/RookFigure.hpp
@@ -0,0 +1,19 @@
+#ifndef INC_MYGAME_CHESS_FIGURE_ROOKFIGURE
+#define INC_MYGAME_CHESS_FIGURE_ROOKFIGURE
+
+#include <mygame/chess/figure/FigureBase.hpp>
+
+namespace chess
+{
+namespace figure
+{
+class RookFigure : public FigureBase
+{
+public:
+    RookFigure();
+};
+
+}  // namespace figure
+}  // namespace chess
+
+#endif  // INC_MYGAME_CHESS_FIGURE_ROOKFIGURE
---
+++ src/chess/figure/RookFigure.cpp
@@ -0,0 +1,13 @@
+#include <mygame/chess/figure/RookFigure.hpp>
+
+namespace chess
+{
+namespace figure
+{
+RookFigure::RookFigure()
+    : FigureBase(FigureType::Rook)
+{
+}
+
+}  // namespace figure
+}  // namespace chess
