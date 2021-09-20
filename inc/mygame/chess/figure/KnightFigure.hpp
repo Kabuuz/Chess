5182a1b2c6c005808251899375d5c6d2b2983777
Konrad Misterski <kmisterski@o2.pl>
2021-09-20 18:39:46 +0200

    Add KnightFigure class

---
+++ inc/mygame/chess/figure/KnightFigure.hpp
@@ -0,0 +1,19 @@
+#ifndef INC_MYGAME_CHESS_FIGURE_KNIGHTFIGURE
+#define INC_MYGAME_CHESS_FIGURE_KNIGHTFIGURE
+
+#include <mygame/chess/figure/FigureBase.hpp>
+
+namespace chess
+{
+namespace figure
+{
+class KnightFigure : public FigureBase
+{
+public:
+    KnightFigure();
+};
+
+}  // namespace figure
+}  // namespace chess
+
+#endif  // INC_MYGAME_CHESS_FIGURE_KNIGHTFIGURE
---
+++ src/chess/figure/KnightFigure.cpp
@@ -0,0 +1,13 @@
+#include <mygame/chess/figure/KnightFigure.hpp>
+
+namespace chess
+{
+namespace figure
+{
+KnightFigure::KnightFigure()
+    : FigureBase(FigureType::Knight)
+{
+}
+
+}  // namespace figure
+}  // namespace chess
