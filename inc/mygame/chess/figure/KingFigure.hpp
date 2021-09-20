9fb339e9dd563c7647e102ad4a61c8fe589c40cd
Konrad Misterski <kmisterski@o2.pl>
2021-09-20 19:00:56 +0200

    Add KingFigure class

---
+++ inc/mygame/chess/figure/KingFigure.hpp
@@ -0,0 +1,19 @@
+#ifndef INC_MYGAME_CHESS_FIGURE_KINGFIGURE
+#define INC_MYGAME_CHESS_FIGURE_KINGFIGURE
+
+#include <mygame/chess/figure/FigureBase.hpp>
+
+namespace chess
+{
+namespace figure
+{
+class KingFigure : public FigureBase
+{
+public:
+    KingFigure();
+};
+
+}  // namespace figure
+}  // namespace chess
+
+#endif  // INC_MYGAME_CHESS_FIGURE_KINGFIGURE
---
+++ src/chess/figure/KingFigure.cpp
@@ -0,0 +1,13 @@
+#include <mygame/chess/figure/KingFigure.hpp>
+
+namespace chess
+{
+namespace figure
+{
+KingFigure::KingFigure()
+    : FigureBase(FigureType::King)
+{
+}
+
+}  // namespace figure
+}  // namespace chess
