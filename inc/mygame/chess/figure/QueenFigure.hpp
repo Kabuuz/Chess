b7f85c4d79d10a82084cdad363bf5b9a330fa6d1
Konrad Misterski <kmisterski@o2.pl>
2021-09-20 18:59:20 +0200

    Add QueenFigure class

---
+++ inc/mygame/chess/figure/QueenFigure.hpp
@@ -0,0 +1,19 @@
+#ifndef INC_MYGAME_CHESS_FIGURE_QUEENFIGURE
+#define INC_MYGAME_CHESS_FIGURE_QUEENFIGURE
+
+#include <mygame/chess/figure/FigureBase.hpp>
+
+namespace chess
+{
+namespace figure
+{
+class QueenFigure : public FigureBase
+{
+public:
+    QueenFigure();
+};
+
+}  // namespace figure
+}  // namespace chess
+
+#endif  // INC_MYGAME_CHESS_FIGURE_QUEENFIGURE
---
+++ src/chess/figure/QueenFigure.cpp
@@ -0,0 +1,13 @@
+#include <mygame/chess/figure/QueenFigure.hpp>
+
+namespace chess
+{
+namespace figure
+{
+QueenFigure::QueenFigure()
+    : FigureBase(FigureType::Queen)
+{
+}
+
+}  // namespace figure
+}  // namespace chess
