d775361b53dd0653bb2ef13d917d741d5cfea698
Konrad Misterski <kmisterski@o2.pl>
2021-09-26 14:28:17 +0200

    Add more tests to FileTests

--- test/ut/tests/chess/table/FileTests.cpp
+++ test/ut/tests/chess/table/FileTests.cpp
@@ -14,31 +14,6 @@ protected:
     }
 };
 
-TEST_F(FileTests, ShouldSaveInteger)
-{
-    const int VALUE = 1;
-    File sut = VALUE;
-
-    EXPECT_EQ(sut, VALUE);
-}
-
-TEST_F(FileTests, ShouldSaveCharLowerCase)
-{
-    const char VALUE = 'a';
-    File sut = VALUE;
-
-    EXPECT_EQ(sut, VALUE);
-}
-
-TEST_F(FileTests, ShouldSaveCharUpperCaseAndReturnLowerCase)
-{
-    const char VALUE_UPPERCASE = 'A';
-    const char VALUE_LOWERCASE = 'a';
-    File sut = VALUE_UPPERCASE;
-
-    EXPECT_EQ(sut, VALUE_LOWERCASE);
-}
-
 TEST_F(FileTests, ShouldReturnMinusOneOnWrongChar)
 {
     const int ERROR = -1;
@@ -46,6 +21,70 @@ TEST_F(FileTests, ShouldReturnMinusOneOnWrongChar)
     File sut = VALUE;
 
     EXPECT_EQ(sut, ERROR);
+}
+
+class FileTestsParametrized : public ::testing::TestWithParam<std::tuple<int, char, char>>
+{
+protected:
+    FileTestsParametrized()
+    {
+    }
+};
+
+INSTANTIATE_TEST_CASE_P(
+    Parameters,
+    FileTestsParametrized,
+    ::testing::Values(
+        std::make_tuple(1, 'a', 'A'),
+        std::make_tuple(2, 'b', 'B'),
+        std::make_tuple(3, 'c', 'C'),
+        std::make_tuple(4, 'd', 'D'),
+        std::make_tuple(5, 'e', 'E'),
+        std::make_tuple(6, 'f', 'F'),
+        std::make_tuple(7, 'g', 'G'),
+        std::make_tuple(8, 'h', 'H')));
+
+TEST_P(FileTestsParametrized, ShouldSaveInteger)
+{
+    const int VALUE = std::get<0>(GetParam());
+    File sut = VALUE;
+
+    EXPECT_EQ(sut, VALUE);
+}
+
+TEST_P(FileTestsParametrized, ShouldSaveCharLowerCase)
+{
+    const char VALUE = std::get<1>(GetParam());
+    File sut = VALUE;
+
+    EXPECT_EQ(sut, VALUE);
+}
+
+TEST_P(FileTestsParametrized, ShouldSaveCharUpperCaseAndReturnLowerCase)
+{
+    const char VALUE_UPPERCASE = std::get<2>(GetParam());
+    const char VALUE_LOWERCASE = std::get<1>(GetParam());
+    File sut = VALUE_UPPERCASE;
+
+    EXPECT_EQ(sut, VALUE_LOWERCASE);
+}
+
+TEST_P(FileTestsParametrized, ShouldConvertUpperCaseToInt)
+{
+    const char VALUE_UPPERCASE = std::get<2>(GetParam());
+    const int VALUE = std::get<0>(GetParam());
+    File sut = VALUE_UPPERCASE;
+
+    EXPECT_EQ(sut, VALUE);
+}
+
+TEST_P(FileTestsParametrized, ShouldConvertLowerCaseToInt)
+{
+    const char VALUE_LOWERCASE = std::get<1>(GetParam());
+    const int VALUE = std::get<0>(GetParam());
+    File sut = VALUE_LOWERCASE;
+
+    EXPECT_EQ(sut, VALUE);
 }
 
 }  // namespace table
